9d8e8067a2be221fc3643dcc91b862241619db9b
Isabel <luisabel@gmail.com>
2013-01-20 21:58:02 +0100

    adding functionality

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -2,19 +2,23 @@ import java.util.Calendar;
 import java.util.Set;
 
 
-public class PastMeeting extends Meeting
+public class PastMeetingImpl implements PastMeeting,Serializable
 {
-
+	private int id;
+	private Calendar date;
+	private Set<Contact> contacts;
 	String notes;
 	
 	public PastMeeting(Set<Contact> contacts,Calendar date)
 	{
-		super(contacts,date);
+		this.contacts=contacts;
+		this.date=date;
 		
 	}
 	public PastMeeting(Set<Contact> contacts,Calendar date,String text)
 	{
-		super(contacts,date);
+		this.contacts=contacts;
+		this.date=date;
 		this.notes=text;
 	}
 
